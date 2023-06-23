3ad8f1284e8633b01612b445f7b3b13547433b19
gain00 <wjdwo1104@gmail.com>
2023-06-23 11:19:43 +0900

    sayHello 메서드 분리

--- src/main/java/hello/spring/basic/Spring5App00.java
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -4,6 +4,12 @@ public class Spring5App00 {
     //첫번재 사례
     //간단한 인삿말을 출력하는 프로그램 작성
     public static void main(String[] args){
+        //sayHello 메서드를 호출하기위해
+        //Spring5App00의 객체화 필요!
+        Spring5App00 a = new Spring5App00();
+        a.sayHello();
+    }
+    private void sayHello(){
         System.out.println("Hello World");
     }
 }
